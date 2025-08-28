d5e9471564ac5f1033f7bd79e41b70890c8eb6e6
Lubna Fatima <lubnafatima965@gmail.com>
2025-08-28 16:36:59 +0000

    var fetch = require('node-fetch'); fetch('https://predict.cogniflow.ai/t

--- src/ai/flows/analyze-text-fraud.ts
+++ src/ai/flows/analyze-text-fraud.ts
@@ -37,7 +37,7 @@ const analyzeTextForFraudFlow = ai.defineFlow(
   },
   async input => {
     const cogniflowApiKey = 'cdc872e5-00ae-4d32-936c-a80bf6a889ce';
-    const cogniflowModelId = '93310e76-d5c0-4856-89e1-af31d18d6a81';
+    const cogniflowModelId = 'b7562ba0-a75d-4001-9375-1f06f22e0b13';
     const url = `https://predict.cogniflow.ai/text/classification/predict/${cogniflowModelId}`;
 
     const response = await fetch(url, {
