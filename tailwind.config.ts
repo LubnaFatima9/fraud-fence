1e7d72c1b3f311276eb55f16e4bec7231505024e
Lubna Fatima <lubnafatima965@gmail.com>
2025-08-29 18:55:08 +0000

    change heading fonts to playfair

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
+        headline: ['"Playfair Display"', 'serif'],
         code: ['monospace'],
       },
       colors: {
